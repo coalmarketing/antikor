cfc81ebce53270f36a03d2514356fe2170137ccd
Michal Špitálský <michal.spitalsky@pm.me>
2025-05-02 11:34:17 +0100

    feat (home): navbar + hero banner initialisation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavbarDesktop from "@/components/navbar";
+import { navigationItems } from "@/data/navbarItems";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavbarDesktop items={navigationItems} />
         {children}
       </body>
     </html>
---
+++ data/navbarItems.tsx
@@ -0,0 +1,11 @@
+export type NavigationItem = {
+  name: string;
+  href: string;
+};
+
+export const navigationItems: NavigationItem[] = [
+  { name: "Domů", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];

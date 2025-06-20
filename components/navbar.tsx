8c3c296d465257851c30ee655a28fdd22a0293f1
Michal Špitálský <michal.spitalsky@pm.me>
2025-06-20 16:56:07 +0100

    style (*): colours setup

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,6 +15,7 @@ const NavbarDesktop: React.FC<NavbarProps> = ({ items }) => {
 
   return (
     <header className="absolute inset-x-0 top-0 z-50">
+      {/* ------ DESKTOP MENU */}
       <nav
         aria-label="Global"
         className="flex items-center justify-between p-6 lg:px-8"
@@ -25,7 +26,7 @@ const NavbarDesktop: React.FC<NavbarProps> = ({ items }) => {
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               alt=""
-              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=steel&shade=600"
               className="h-8 w-auto"
             />
           </a>
@@ -34,7 +35,7 @@ const NavbarDesktop: React.FC<NavbarProps> = ({ items }) => {
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-light bg-steel-700 hover:bg-steel-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-steel"
           >
             <span className="sr-only">Otevřít menu</span>
             <Bars3Icon aria-hidden="true" className="size-6" />
@@ -45,7 +46,7 @@ const NavbarDesktop: React.FC<NavbarProps> = ({ items }) => {
             <a
               key={item.name}
               href={item.href}
-              className="text-sm/6 font-semibold text-gray-900"
+              className="text-sm/6 font-semibold text-light"
             >
               {item.name}
             </a>
@@ -54,33 +55,36 @@ const NavbarDesktop: React.FC<NavbarProps> = ({ items }) => {
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a
             href="#"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-steel-700 px-3.5 py-2.5 text-sm font-semibold text-light shadow-xs hover:bg-steel-800 transition focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-steel-600"
           >
             Get started &#x2964;
           </a>
         </div>
       </nav>
+
+      {/* ------ MOBILE MENU */}
+
       <Dialog
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
         className="lg:hidden"
       >
         <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-dark px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-light/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">ANTIKOR</span>
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
                 alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=steel&shade=600"
                 className="h-8 w-auto"
               />
             </a>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 rounded-md p-2.5 text-light"
             >
               <span className="sr-only">Zavřít menu</span>
               <XMarkIcon aria-hidden="true" className="size-6" />
@@ -93,7 +97,7 @@ const NavbarDesktop: React.FC<NavbarProps> = ({ items }) => {
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-light"
                   >
                     {item.name}
                   </a>
@@ -102,9 +106,9 @@ const NavbarDesktop: React.FC<NavbarProps> = ({ items }) => {
               <div className="py-6">
                 <a
                   href="#"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="rounded-md bg-steel-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-steel-800 transition focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-steel-600"
                 >
-                  Get started &#x2964;
+                  Kup ocel &#x2964;
                 </a>
               </div>
             </div>

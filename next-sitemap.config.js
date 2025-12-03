0a68673caffd8e2939f1037e0cbcc2441b4b049e
Michal Špitálský <michal.spitalsky@pm.me>
2025-12-03 14:54:48 +0100

    feat (*): performance tweaks + sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://antikor-zdenek-maixner.netlify.app/', // Replace with your actual domain
+  generateRobotsTxt: true,            // Generate robots.txt automatically
+  sitemapSize: 7000                   // Optional
+};
